5cf0adcc9165f63d188ad1c3b6729e0b41662c9e
Tim Eccleston <teccleston@axialhealthcare.com>
2018-05-02 02:16:14 -0500

    add tests for second example

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 import graph_isomorphism as gi
 
+######## Simplest Example ########
 edge_set_graph = (
   (0, 1),
   (1, 2),
@@ -20,6 +21,22 @@ permutations = [
 redundant_graph = [a for a in reversed(edge_set_graph)] + list(edge_set_graph)
 
 node_degree = {1: [0, 2], 2: [1]}
+
+######## Second Example ########
+edge_set_graph2 = (
+  (0, 1),
+  (1, 2),
+  (2, 3),
+)
+
+node_degree2 = {1: [0, 3], 2: [1, 2]}
+
+node_degree_permutations2 = [
+    {0: 0, 1: 1, 2: 2, 3: 3},
+    {0: 0, 1: 2, 2: 1, 3: 3},
+    {0: 3, 1: 1, 2: 2, 3: 0},
+    {0: 3, 1: 2, 2: 1, 3: 0},
+]
 
 class bq_tests(unittest.TestCase):
     def test001_collect_nodes(self):
@@ -60,17 +77,30 @@ class bq_tests(unittest.TestCase):
             [permutations[0], permutations[-1]],
             gi.find_homomorphisms(edge_set_graph, permutations)
         )
+        self.assertEqual(
+            [node_degree_permutations2[0], node_degree_permutations2[-1]],
+            gi.find_homomorphisms(edge_set_graph2, node_degree_permutations2)
+        )
 
     def test_007_degree_nodes(self):
         self.assertEqual(
             node_degree,
             gi.degree_nodes(edge_set_graph)
         )
+        self.assertEqual(
+            node_degree2,
+            gi.degree_nodes(edge_set_graph2)
+        )
 
     def test_008_create_node_degree_permutations(self):
         self.assertEqual(
             permutations[:1] + permutations[-1:],
             gi.create_node_degree_permutations(node_degree)
+        )
+
+        self.assertEqual(
+            node_degree_permutations2,
+            gi.create_node_degree_permutations(node_degree2)
         )
 
 if __name__ == '__main__':
