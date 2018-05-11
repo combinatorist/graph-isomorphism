c3e99bb6abe7ecff5f48ebda14ccc8a42600e001
Tim Eccleston <teccleston@axialhealthcare.com>
2018-05-11 07:00:50 -0500

    draft find_all_graphs with hashable datatypes (:warning: groups homomorphisms, need isomorphisms)

--- graph_isomorphism.py
+++ graph_isomorphism.py
@@ -61,28 +61,44 @@ def powerset(raw_set):
     return frozenset(power_generator)
 
 def find_all_graphs_up_to_order(order):
-    """ Generates all edge_sets and groups into isomorphism classes
+    """ Generates all edge_sets and groups them into isomorphism classes
     """
 
-    def degree_counts(edge_set):
-        return {k: len(list(v)) for k, v in degree_nodes(edge_set).items()}
+    def degree_counts(degree_nodes):
+        return {k: len(list(v)) for k, v in degree_nodes.items()}
 
     nodes = range(order)
     possible_edges = frozenset(combinations(nodes, 2))
     edge_sets = powerset(possible_edges)
 
     # group edge_sets by identifying topological characteristics
-    analyzed = {edge_set: degree_counts(edge_set) for edge_set in edge_sets}
+    analyzed = {edge_set: degree_nodes(edge_set) for edge_set in edge_sets}
+    node_degree_counts = {k: degree_counts(v) for k, v in analyzed.items()}
 
-    grouped_by_degree_counts = defaultdict(set)
-    for k, v in analyzed.items():
-        grouped_by_degree_counts[frozenset(v.items())].add(k)
+    grouped_by_degree_counts = defaultdict(list)
+    for k, v in node_degree_counts.items():
+        grouped_by_degree_counts[frozenset(v.items())].append((k, analyzed[k]))
 
     # subgroup edge_sets into isomorphic equivalence classes
     subgrouped_by_isomorphism = dict()
-    for k, edge_sets in grouped_by_degree_counts.items():
-        new_edge_sets = list(list(edge_sets[0]))
-        for edge_set in edge_sets[1:]:
-            if edge_set
+    for k, analyzed_edge_sets in grouped_by_degree_counts.items():
+        new_edge_sets = [[analyzed_edge_sets[0][0]]]
+        for edge_set, node_degrees in analyzed_edge_sets[1:]:
+            is_isomorphic = False
 
-    return grouped_by_degree_counts ## WIP: intermediate result
+            #:warning: Finding homomorphic permutations, but need isomorphisms
+            # we need to map from graph A to B based on node degree.
+            # so, A's degree 1 nodes to B's degree 1 nodes, etc
+            for permutation in create_node_degree_permutations(node_degrees):
+                for edge_set_class in new_edge_sets:
+                    if is_isomorphism(permutation, edge_set, edge_set_class[0]):
+                        is_isomorphic = True
+                        edge_set_class.append(edge_set)
+                        break
+                if is_isomorphic:
+                    break
+            if not is_isomorphic:
+                new_edge_sets.append([edge_set])
+        subgrouped_by_isomorphism[k] = new_edge_sets
+
+    return subgrouped_by_isomorphism
